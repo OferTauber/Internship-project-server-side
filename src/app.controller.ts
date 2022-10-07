088a7ae6779e1043c1f6cb93b90855f9d9c61072
Ofer Tauber <ofertauber@gmail.com>
2022-10-07 14:23:43 +0300

    Re marge

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,5 @@
 import { Controller, Get, Put } from '@nestjs/common';
 import { AppService } from './app.service';
-import run, {
-  getUserFromDB,
-  getUserById,
-  addTokenToUser,
-  uploadPortsChancks,
-  getAllFirstOrderPortsFromDB,
-  uploadSecondOrderPorts,
-  getSecondOrderPort,
-} from './utils/mogo';
-import * as bcrypt from 'bcrypt';
 
 @Controller()
 export class AppController {
@@ -19,18 +9,4 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
-
-  // @Put()
-  // async test(): Promise<any> {
-  //   const user = await getUserById('633ab0d8ed09597be1ae72ef');
-  //   return await getSecondOrderPort('wld_trs_ports_wfp.14315');
-  //   return await uploadSecondOrderPorts();
-  //   return await getAllFirstOrderPorts();
-  //   return await uploadPortsChancks();
-  //   return await addTokenToUser(user, '123');
-  //   return await getUserById('633ab0d8ed09597be1ae72ef');
-  //   return await getUserByEmail('user@email.com');
-  //   return await run();
-  //   return await bcrypt.hash('123', 10);
-  // }
 }
